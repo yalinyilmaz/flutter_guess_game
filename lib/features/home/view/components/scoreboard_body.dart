29b5d4e20451bb3d2622bff92d5792e18ef78876
yalin yilmaz <yalinyilmaz96@hotmail.com>
2024-10-02 21:30:23 +0300

    scoreboard design was updated

--- lib/features/home/view/components/scoreboard_body.dart
+++ lib/features/home/view/components/scoreboard_body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_guess_game/app/navigation/router.dart';
+import 'package:flutter_guess_game/app/theme/new_theme.dart';
 
 class ScoreboardBody extends StatelessWidget {
   const ScoreboardBody({super.key});
@@ -11,12 +13,20 @@ class ScoreboardBody extends StatelessWidget {
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: Table(
-            border: TableBorder.all(color: Colors.white),
+            border:
+                TableBorder.all(color: globalCtx.whiteColor.shade100, width: 2),
+            columnWidths: const {
+              0: FlexColumnWidth(1),
+              1: FlexColumnWidth(1),
+              2: FlexColumnWidth(3),
+            },
             children: [
-              _buildTableRow(['Games', '1st', '2nd', '3rd', '4th', '5th'],
-                  isHeader: true),
-              _buildTableRow(['Score', "6", "12", "32", "4", "3"]),
-              _buildTableRow(['Tries', "6", "12", "32", "4", "1"]),
+              _buildTableRow(['Games', 'Scores', 'Tries'], isHeader: true),
+              _buildTableRow(['1st', "72", "liste gelecek"]),
+              _buildTableRow(['2nd', "66", "liste gelecek"]),
+              _buildTableRow(['3rd', "24", "liste gelecek"]),
+              _buildTableRow(['4th', "13", "liste gelecek"]),
+              _buildTableRow(['5th', "45", "liste gelecek"]),
             ],
           ),
         ),
@@ -29,15 +39,15 @@ class ScoreboardBody extends StatelessWidget {
       children: cells
           .map((cell) => TableCell(
                 child: Container(
-                  padding: const EdgeInsets.all(8.0),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 4.0, vertical: 20),
                   color: Colors.transparent,
                   child: Text(
                     cell,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight:
-                          isHeader ? FontWeight.bold : FontWeight.normal,
-                    ),
+                    style: globalCtx.textTheme.bodyEmphasized.copyWith(
+                        color: isHeader
+                            ? globalCtx.yellowColor.shade900
+                            : globalCtx.whiteColor.shade500),
                     textAlign: TextAlign.center,
                   ),
                 ),
